e9c209408835f4e12fb14d2567d5b0b92f2718fe
Steven Chang <stevenchang5000@protonmail.com>
2023-09-16 07:29:40 +1000

    Remove rating

--- lib.rs
+++ lib.rs
@@ -10,7 +10,6 @@ mod az_smart_contract_hub {
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum AZSmartContractHubError {
         NotFound(String),
-        OutOfRange(String),
         Unauthorised,
         Unchanged(String),
     }
@@ -35,16 +34,6 @@ mod az_smart_contract_hub {
         enabled: bool,
     }
 
-    #[ink(event)]
-    pub struct Rate {
-        #[ink(topic)]
-        id: u32,
-        previous_user_rating: i8,
-        new_user_rating: i8,
-        #[ink(topic)]
-        user: AccountId,
-    }
-
     // === STRUCTS ===
     #[derive(scale::Decode, scale::Encode, Debug, Clone, PartialEq)]
     #[cfg_attr(
@@ -56,8 +45,6 @@ mod az_smart_contract_hub {
         smart_contract_address: AccountId,
         url: String,
         environment: u8,
-        likes: u16,
-        dislikes: u16,
         caller: AccountId,
         enabled: bool,
     }
@@ -68,7 +55,6 @@ mod az_smart_contract_hub {
         azero_id_address: AccountId,
         smart_contracts: Mapping<u32, SmartContract>,
         smart_contracts_count: u32,
-        user_ratings: Mapping<(u32, AccountId), i8>,
     }
     impl AZSmartContractHub {
         #[ink(constructor)]
@@ -77,7 +63,6 @@ mod az_smart_contract_hub {
                 azero_id_address,
                 smart_contracts: Mapping::default(),
                 smart_contracts_count: 0,
-                user_ratings: Mapping::default(),
             }
         }
 
@@ -110,16 +95,12 @@ mod az_smart_contract_hub {
                 smart_contract_address,
                 url: url.clone(),
                 environment,
-                likes: 1,
-                dislikes: 0,
                 caller: Self::env().caller(),
                 enabled: true,
             };
             self.smart_contracts
                 .insert(self.smart_contracts_count, &smart_contract);
             self.smart_contracts_count += 1;
-
-            self.user_ratings.insert((smart_contract.id, caller), &1);
 
             // emit event
             self.env().emit_event(Create {
@@ -128,55 +109,6 @@ mod az_smart_contract_hub {
                 url,
                 environment,
                 caller,
-            });
-
-            Ok(smart_contract)
-        }
-
-        #[ink(message)]
-        pub fn rate(
-            &mut self,
-            id: u32,
-            new_user_rating: i8,
-        ) -> Result<SmartContract, AZSmartContractHubError> {
-            let mut smart_contract: SmartContract = self.show(id)?;
-            if !(-1..=1).contains(&new_user_rating) {
-                return Err(AZSmartContractHubError::OutOfRange("Rating".to_string()));
-            }
-
-            let caller: AccountId = Self::env().caller();
-            let previous_user_rating: i8 = self.user_ratings.get((id, caller)).unwrap_or(0);
-            if previous_user_rating == new_user_rating {
-                return Err(AZSmartContractHubError::Unchanged("Rating".to_string()));
-            }
-
-            if new_user_rating == -1 {
-                if previous_user_rating == 1 {
-                    smart_contract.likes -= 1
-                }
-                smart_contract.dislikes += 1
-            } else if new_user_rating == 0 {
-                if previous_user_rating == 1 {
-                    smart_contract.likes -= 1
-                } else if previous_user_rating == -1 {
-                    smart_contract.dislikes -= 1
-                }
-            } else {
-                if previous_user_rating == -1 {
-                    smart_contract.dislikes -= 1
-                }
-                smart_contract.likes += 1
-            }
-            self.smart_contracts
-                .insert(smart_contract.id, &smart_contract);
-            self.user_ratings.insert((id, caller), &new_user_rating);
-
-            // emit event
-            self.env().emit_event(Rate {
-                id: smart_contract.id,
-                previous_user_rating,
-                new_user_rating,
-                user: caller,
             });
 
             Ok(smart_contract)
@@ -219,7 +151,7 @@ mod az_smart_contract_hub {
             DefaultEnvironment,
         };
 
-        const MOCK_URL: &str = "https://res.cloudinary.com/hv5cxagki/raw/upload/v1690808298/smart_contract_metadata/tmuurccd5a7lcvin6ae9.json";
+        const MOCK_URL: &str = "https://res.cloudinary.com/xasdf123/raw/upload/v1690808298/smart_contract_metadata/tmuurccd5a7lcvin6ae9.json";
 
         // === HELPERS ===
         fn init() -> (DefaultAccounts<DefaultEnvironment>, AZSmartContractHub) {
@@ -268,173 +200,12 @@ mod az_smart_contract_hub {
             assert_eq!(az_smart_contract_hub.smart_contracts_count, u32::MAX);
             // * it stores the submitted smart contract address
             assert_eq!(result_unwrapped.smart_contract_address, accounts.alice);
-            // * it sets the like to 1 and dislike to 0
-            assert_eq!(result_unwrapped.likes, 1);
-            assert_eq!(result_unwrapped.dislikes, 0);
             // * it sets the caller to the caller
             assert_eq!(result_unwrapped.caller, accounts.bob);
-            // * it sets the user_rating to 1
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((result_unwrapped.id, result_unwrapped.caller))
-                    .unwrap(),
-                1
-            );
             // * it stores the smart_contract
             assert_eq!(
                 result_unwrapped,
                 az_smart_contract_hub.show(result_unwrapped.id).unwrap()
-            );
-        }
-
-        #[ink::test]
-        fn test_rate() {
-            let (accounts, mut az_smart_contract_hub) = init();
-            // = when smart_contract doesn't exist
-            // = * it raises an error
-            let mut result = az_smart_contract_hub.rate(0, 0);
-            assert_eq!(
-                result,
-                Err(AZSmartContractHubError::NotFound(
-                    "SmartContract".to_string()
-                ))
-            );
-
-            // = when smart_contract exists
-            az_smart_contract_hub
-                .create(accounts.alice, MOCK_URL.to_string(), 1)
-                .unwrap();
-            // == when new rating is less than -1
-            result = az_smart_contract_hub.rate(0, -2);
-            // == * it raises an error
-            assert_eq!(
-                result,
-                Err(AZSmartContractHubError::OutOfRange("Rating".to_string()))
-            );
-            // == when new rating is grater than 1
-            result = az_smart_contract_hub.rate(0, 2);
-            // == * it raises an error
-            assert_eq!(
-                result,
-                Err(AZSmartContractHubError::OutOfRange("Rating".to_string()))
-            );
-            // == when new rating is within range
-            // === when new rating is the same as user's current rating
-            result = az_smart_contract_hub.rate(0, 1);
-            // === * it raises an error
-            assert_eq!(
-                result,
-                Err(AZSmartContractHubError::Unchanged("Rating".to_string()))
-            );
-
-            // === when new rating is different to user's current rating
-            // ==== when current rating is 1
-            // ===== when new rating is 0
-            let mut new_user_rating: i8 = 0;
-            result = az_smart_contract_hub.rate(0, new_user_rating);
-            let mut smart_contract = result.unwrap();
-            // ===== * it reduces the smart_contract's likes by 1
-            // ===== * it does not change the dislikes
-            // ===== * it sets the user's rating for the smart_contract to 1
-            assert_eq!(smart_contract.likes, 0);
-            assert_eq!(smart_contract.dislikes, 0);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
-            );
-
-            // ===== when new rating is -1
-            az_smart_contract_hub.rate(0, 1).unwrap();
-            new_user_rating = -1;
-            smart_contract = az_smart_contract_hub.rate(0, new_user_rating).unwrap();
-            // ===== * it reduces the smart_contract's likes by 1
-            // ===== * it increases the smart_contract's dislikes by 1
-            // ===== * it sets the user's rating for the smart_contract to -1
-            assert_eq!(smart_contract.likes, 0);
-            assert_eq!(smart_contract.dislikes, 1);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
-            );
-
-            // ==== when current rating is -1
-            // ===== when new rating is 0
-            new_user_rating = 0;
-            result = az_smart_contract_hub.rate(0, new_user_rating);
-            smart_contract = result.unwrap();
-            // ===== * it reduces the smart_contract's dislikes by 1
-            // ===== * it does not change the likes
-            // ===== * it sets the user's rating for the smart_contract to 0
-            assert_eq!(smart_contract.likes, 0);
-            assert_eq!(smart_contract.dislikes, 0);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
-            );
-
-            // ===== when new rating is 1
-            az_smart_contract_hub.rate(0, -1).unwrap();
-            new_user_rating = 1;
-            result = az_smart_contract_hub.rate(0, new_user_rating);
-            smart_contract = result.unwrap();
-            // ===== * it reduces the smart_contract's dislikes by 1
-            // ===== * it increases the likes by 1
-            // ===== * it sets the user's rating for the smart_contract to 1
-            assert_eq!(smart_contract.likes, 1);
-            assert_eq!(smart_contract.dislikes, 0);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
-            );
-
-            // ==== when current rating is 0
-            az_smart_contract_hub.rate(0, 0).unwrap();
-            // ===== when new rating is 1
-            new_user_rating = 1;
-            result = az_smart_contract_hub.rate(0, new_user_rating);
-            smart_contract = result.unwrap();
-            // ===== * it does not change the dislikes
-            // ===== * it increases the likes by 1
-            // ===== * it sets the user's rating for the smart_contract to 1
-            assert_eq!(smart_contract.likes, 1);
-            assert_eq!(smart_contract.dislikes, 0);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
-            );
-
-            az_smart_contract_hub.rate(0, 0).unwrap();
-            // ===== when new rating is -1
-            new_user_rating = -1;
-            result = az_smart_contract_hub.rate(0, new_user_rating);
-            smart_contract = result.unwrap();
-            // ===== * it increases the dislikes by 1
-            // ===== * it does not change the likes
-            // ===== * it sets the user's rating for the smart_contract to -1
-            assert_eq!(smart_contract.likes, 0);
-            assert_eq!(smart_contract.dislikes, 1);
-            assert_eq!(
-                az_smart_contract_hub
-                    .user_ratings
-                    .get((0, accounts.bob))
-                    .unwrap_or(0),
-                new_user_rating
             );
         }
 
